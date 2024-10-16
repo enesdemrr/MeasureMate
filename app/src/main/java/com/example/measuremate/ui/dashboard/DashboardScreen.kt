a6a4ec9b28ab58e9c8af24ea2015d10693b15958
enesdemir <enesdemr@outlook.com.tr>
2024-10-16 15:27:06 +0300

    update DashboardScreen

--- app/src/main/java/com/example/measuremate/ui/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/example/measuremate/ui/dashboard/DashboardScreen.kt
@@ -1,25 +1,73 @@
 package com.example.measuremate.ui.dashboard
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
+import com.example.measuremate.domain.model.BodyPart
+import com.example.measuremate.domain.model.predefinedBodyParts
 import com.example.measuremate.ui.component.ProfilePicturePlace
+import com.example.measuremate.ui.theme.MeasureMateTheme
 
 @Composable
 fun DashboardScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        DashboardTopBar(
-            profilePictureUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg",
-            onProfilePictureClick = {}
-        )
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            DashboardTopBar(
+                profilePictureUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg",
+                onProfilePictureClick = {}
+            )
+            LazyVerticalGrid(
+                modifier = Modifier.fillMaxSize(),
+                columns = GridCells.Adaptive(minSize = 300.dp),
+                contentPadding = PaddingValues(16.dp, top = 8.dp, 16.dp, 16.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                horizontalArrangement = Arrangement.spacedBy(32.dp),
+            ) {
+                items(predefinedBodyParts) { bodyPart ->
+                    ItemCard(bodyPart = bodyPart)
+                }
+            }
+        }
+        FloatingActionButton(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(24.dp),
+            onClick = { TODO() }
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Add Icon")
+        }
     }
 }
 
@@ -45,12 +93,55 @@ private fun DashboardTopBar(
 
         }
     )
+}
 
+@Composable
+private fun ItemCard(
+    modifier: Modifier = Modifier,
+    bodyPart: BodyPart
+) {
+    Card(
+        modifier = modifier,
+        onClick = { TODO() })
+    {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier.weight(8f),
+                text = bodyPart.name,
+                style = MaterialTheme.typography.bodyLarge,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "${bodyPart.latestValue ?: ""} ${bodyPart.measuringUnit}",
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Box(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.surface)
+                    .padding(4.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowForward,
+                    contentDescription = "Show Details",
+                    modifier = Modifier.size(15.dp)
+                )
+            }
+        }
+    }
 }
 
 
 @PreviewScreenSizes
 @Composable
 fun DashboardScreenPreview() {
-    DashboardScreen()
+    MeasureMateTheme {
+        DashboardScreen()
+    }
 }
